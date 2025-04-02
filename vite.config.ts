3cfd6f886cb4c1ab9ca4013734cd1b49c2b414af
Christian Liebenberg <110298095+christian2001za@users.noreply.github.com>
2025-04-02 11:35:26 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,32 +1,12 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
+import react from '@vitejs/plugin-react';
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  server: {
-    host: "::",
-    port: 8080,
-  },
-  plugins: [
-    react(),
-    mode === 'development' &&
-    componentTagger(),
-  ].filter(Boolean),
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
+  base: '/',
+  plugins: [react()],
 }));
 
-// vite.config.ts
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-export default defineConfig({
-  base: '/', // ← change this if your domain is a subpath (but for custom domain, keep it as '/')
-  plugins: [react()],
-})
 
